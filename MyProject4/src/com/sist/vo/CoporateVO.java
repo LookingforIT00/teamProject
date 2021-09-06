072c78adfc6802edc64b4c756b3cb6a38e6129ae
Learn0 <kim852654@gmail.com>
2021-09-06 17:34:56 +0900

    Commit

--- MyProject4/src/com/sist/vo/CoporateVO.java
+++ MyProject4/src/com/sist/vo/CoporateVO.java
@@ -1,10 +1,5 @@
 package com.sist.vo;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 public class CoporateVO {
 	
 	String coName;
@@ -14,4 +9,46 @@ public class CoporateVO {
 	String coSales;
 	String coLink;
 	Integer coWorkers;
+	public String getCoName() {
+		return coName;
+	}
+	public void setCoName(String coName) {
+		this.coName = coName;
+	}
+	public String getCoType() {
+		return coType;
+	}
+	public void setCoType(String coType) {
+		this.coType = coType;
+	}
+	public String getCoScale() {
+		return coScale;
+	}
+	public void setCoScale(String coScale) {
+		this.coScale = coScale;
+	}
+	public String getCoYear() {
+		return coYear;
+	}
+	public void setCoYear(String coYear) {
+		this.coYear = coYear;
+	}
+	public String getCoSales() {
+		return coSales;
+	}
+	public void setCoSales(String coSales) {
+		this.coSales = coSales;
+	}
+	public String getCoLink() {
+		return coLink;
+	}
+	public void setCoLink(String coLink) {
+		this.coLink = coLink;
+	}
+	public Integer getCoWorkers() {
+		return coWorkers;
+	}
+	public void setCoWorkers(Integer coWorkers) {
+		this.coWorkers = coWorkers;
+	}
 }
--- MyProject4/src/com/sist/vo/JobVO.java
+++ MyProject4/src/com/sist/vo/JobVO.java
@@ -1,10 +1,5 @@
 package com.sist.vo;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 public class JobVO {
 	
 	private Integer idx;
@@ -14,6 +9,102 @@ public class JobVO {
 	private String addr;
 	private String sal;
 	private String empType;
+	public Integer getIdx() {
+		return idx;
+	}
+	public void setIdx(Integer idx) {
+		this.idx = idx;
+	}
+	public String getJobName() {
+		return jobName;
+	}
+	public void setJobName(String jobName) {
+		this.jobName = jobName;
+	}
+	public String getEdu() {
+		return edu;
+	}
+	public void setEdu(String edu) {
+		this.edu = edu;
+	}
+	public String getCareer() {
+		return career;
+	}
+	public void setCareer(String career) {
+		this.career = career;
+	}
+	public String getAddr() {
+		return addr;
+	}
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+	public String getSal() {
+		return sal;
+	}
+	public void setSal(String sal) {
+		this.sal = sal;
+	}
+	public String getEmpType() {
+		return empType;
+	}
+	public void setEmpType(String empType) {
+		this.empType = empType;
+	}
+	public String getWorkType() {
+		return workType;
+	}
+	public void setWorkType(String workType) {
+		this.workType = workType;
+	}
+	public String getWorkTime() {
+		return workTime;
+	}
+	public void setWorkTime(String workTime) {
+		this.workTime = workTime;
+	}
+	public String getWelfare() {
+		return welfare;
+	}
+	public void setWelfare(String welfare) {
+		this.welfare = welfare;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getStartline() {
+		return startline;
+	}
+	public void setStartline(String startline) {
+		this.startline = startline;
+	}
+	public String getDeadline() {
+		return deadline;
+	}
+	public void setDeadline(String deadline) {
+		this.deadline = deadline;
+	}
+	public String getReception() {
+		return reception;
+	}
+	public void setReception(String reception) {
+		this.reception = reception;
+	}
+	public Integer getHit() {
+		return hit;
+	}
+	public void setHit(Integer hit) {
+		this.hit = hit;
+	}
+	public String getCoName() {
+		return coName;
+	}
+	public void setCoName(String coName) {
+		this.coName = coName;
+	}
 	private String workType;
 	private String workTime;
 	private String welfare;
