a21e77f947995106fc8b91c6649783cfce792f45
박성경 <@DESKTOP-V401DVT>
2021-09-15 09:12:01 +0900

    111

--- MyProject4/src/com/sist/dao/MemberDAO.java
+++ MyProject4/src/com/sist/dao/MemberDAO.java
@@ -10,17 +10,13 @@ import javax.naming.*;
 public class MemberDAO {
   private Connection conn;
   private PreparedStatement ps;
-  private static MemberDAO dao; // 싱글턴 패턴 (static:메모리 공간을 한개만 사용이 가능) => 재사용 
-  // Connection 주소 읽기 => Connection객체를 미리생성 => 생성시간을 줄여서 속도가 빠르게 만든다 
-  // 갯수 지정을 했기에 Connection객체 관리가 편하다 => 모든 웹사이트 개발는 95%가 DBCP를 사용한다 
-  // Connection 객체 얻기 
+  private static MemberDAO dao; 
   public void getConnection()
   {
 	  try
 	  {
-		  Context init=new InitialContext(); // 저장된 위치에 접근 
-			// 저장된 위치 이름 = JNDI (java naming directory interface)
-			Context c=(Context)init.lookup("java://comp//env"); // 찾아오기
+		  Context init=new InitialContext(); 
+			Context c=(Context)init.lookup("java://comp//env"); 
 			DataSource ds=(DataSource)c.lookup("jdbc/oracle");
 			  conn=ds.getConnection();
 		  }catch(Exception ex) 
@@ -28,7 +24,6 @@ public class MemberDAO {
 			  ex.printStackTrace();
 		  }
 	  }
-	  // 사용후 반환 -> 다른사람이 재사용이 가능하게 만든다 
 	  public void disConnection()
 	  {
 		  try
@@ -40,9 +35,9 @@ public class MemberDAO {
 	  // 싱글턴 패턴 
 	  public static MemberDAO newInstance()
 	  {
-		  if(dao==null) // 미생성시에는 
+		  if(dao==null) 
 			  dao=new MemberDAO();
-		  return dao; // 이미 만들어진 dao객체를 사용한다 
+		  return dao; 
 			  
 	  }
 	// 첫 번째 기능 => 아이디 중복 체크
@@ -52,9 +47,8 @@ public class MemberDAO {
 		  try
 		  {
 			  getConnection();
-			  String sql="SELECT COUNT(*) FROM project_member "
+			  String sql="SELECT COUNT(*) FROM member "
 					    +"WHERE id=?";
-			  // count=0 (사용가능한 ID), count=1 (사용중인 ID)
 			  ps=conn.prepareStatement(sql);
 			  ps.setString(1,id);
 			  ResultSet rs=ps.executeQuery();
@@ -71,7 +65,6 @@ public class MemberDAO {
 		  }
 		  return count;
 	  }
-	  // 기능 => 우편번호 검색 
 	  public List<ZipcodeVO> postfind(String dong)
 	  {
 		  List<ZipcodeVO> list=new ArrayList<ZipcodeVO>();
@@ -136,7 +129,7 @@ public class MemberDAO {
 		  try
 		  {
 			  getConnection();
-			  String sql="INSERT INTO project_member VALUES(?,?,?,?,?,"
+			  String sql="INSERT INTO member VALUES(?,?,?,?,?,"
 					   +"?,?,?,?,?,'n')"; // admin(n=일반,y=관리자)
 			  ps=conn.prepareStatement(sql);
 			  ps.setString(1, vo.getId());
@@ -150,6 +143,8 @@ public class MemberDAO {
 			  ps.setString(8, vo.getAddr1());
 			  ps.setString(9, vo.getAddr2());
 			  ps.setString(10, vo.getTel());
+			  ps.setString(10, vo.getHope_region());
+			  ps.setString(10, vo.getHope_job());
 			  
 			  ps.executeUpdate();
 		  }catch(Exception ex)
@@ -169,22 +164,22 @@ public class MemberDAO {
 		  {
 			  getConnection();
 			  String sql="SELECT COUNT(*) "
-					    +"FROM project_member "
+					    +"FROM member "
 					    +"WHERE id=?";
-			  ps=conn.prepareStatement(sql); //id가 존재하는지 체크
+			  ps=conn.prepareStatement(sql); 
 			  ps.setString(1, id);
 			  ResultSet rs=ps.executeQuery();
 			  rs.next();
 			  int count=rs.getInt(1);
 			  rs.close();
 			  
-			  if(count==0) //ID가 없는 상태
+			  if(count==0) 
 			  {
 				  result="NOID";
 			  }
-			  else // ID가 있는 상태
+			  else 
 			  {
-				  sql="SELECT pwd,name,admin FROM project_member "
+				  sql="SELECT pwd,name,admin FROM member "
 					 +"WHERE id=?";
 				  ps=conn.prepareStatement(sql);
 				  ps.setString(1, id);
@@ -194,8 +189,7 @@ public class MemberDAO {
 				  String name=rs.getString(2);
 				  String admin=rs.getString(3);
 				  rs.close();
-				  // 비밀번호 확인 
-				  if(db_pwd.equals(pwd))//로그인
+				  if(db_pwd.equals(pwd))
 				  {
 					  result=name+"|"+admin;
 				  }
